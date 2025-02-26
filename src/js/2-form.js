385e504508636588ae5bcc499fc6803efacb8c04
Tania Dumych <tanja11.malvchko@gmail.com>
2025-02-26 21:05:40 +0100

    new js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,21 +1,21 @@
 const form = document.querySelector('.feedback-form');
-const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const STORAGE_KEY = 'feedback-form-state';
 
 let formData = {
   email: '',
-  message: '',
+  message: ''
 };
 
-const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+const savedData = localStorage.getItem(STORAGE_KEY);
 if (savedData) {
-  formData = JSON.parse(savedData);
+  Object.assign(formData, JSON.parse(savedData));
   form.elements.email.value = formData.email;
   form.elements.message.value = formData.message;
 }
 
 form.addEventListener('input', event => {
   formData[event.target.name] = event.target.value.trim();
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
 
 form.addEventListener('submit', event => {
@@ -26,7 +26,11 @@ form.addEventListener('submit', event => {
     return;
   }
 
-  localStorage.removeItem(LOCAL_STORAGE_KEY);
-  form.reset();
-  formData = { email: '', message: '' };
+  console.log(formData); 
+
+  localStorage.removeItem(STORAGE_KEY); 
+  event.target.reset(); 
+  
+  formData.email = '';
+  formData.message = '';
 });
